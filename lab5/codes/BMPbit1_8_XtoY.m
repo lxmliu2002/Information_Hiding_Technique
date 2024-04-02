0151d895e5f377d6d5572a1bd1d51c326510adff
lxmliu2002 <lxmliu2002@126.com>
2024-04-02 08:45:29 +0800

    fix(lab5): complete lab5

--- lab5/codes/BMPbit1_8_XtoY.m
+++ lab5/codes/BMPbit1_8_XtoY.m
@@ -10,7 +10,9 @@ imwrite(img, './pic/BMPbit1_8_XtoY/NKU70_gray.bmp');
 
 for t = 1 : 8
     [m, n] = size(img);
+    x = zeros(m, n);
     y = zeros(m, n);
+    z = zeros(m, n);
     for i = 1 : m
         for j = 1 : n
             x(i, j) = bitget(img(i, j), t);
@@ -20,16 +22,47 @@ for t = 1 : 8
     figure;
     imshow(x, []);
     title(['第 ', num2str(t), ' 个位平面']);
-    imwrite(x, ['./pic/BMPbit1_8_XtoY/NKU70_gray_', num2str(t), '.bmp']);
+    imwrite(x, ['./pic/BMPbit1_8_XtoY/NKU70_gray_', num2str(t), '.png']);
 
-    for i = 1 : m
-        for j = 1 : n
-            y(i, j) = bitset(y(i, j), t, x(i, j));
+
+    for k = 1 : t
+        x = zeros(m, n);
+
+        for i = 1 : m
+            for j = 1 : n
+                x(i, j) = bitget(img(i, j), k);
+            end
+        end
+
+        for i = 1 : m
+            for j = 1 : n
+                y(i, j) = bitset(y(i, j), k, x(i, j));
+            end
         end
     end
 
     figure;
     imshow(y, []);
     title(['第 1 - ', num2str(t), ' 个位平面']);
-    imwrite(y, ['./pic/BMPbit1_8_XtoY/NKU70_gray_', num2str(t), '_rebuild.bmp']);
+    imwrite(y, ['./pic/BMPbit1_8_XtoY/NKU70_gray_lo_', num2str(t), '_rebuild.png']);
+
+    for k = t + 1 : 8
+        x = zeros(m, n);
+        for i = 1 : m
+            for j = 1 : n
+                x(i, j) = bitget(img(i, j), k);
+            end
+        end
+        for i = 1 : m
+            for j = 1 : n
+                z(i, j) = bitset(z(i, j), k, x(i, j));
+            end
+        end
+    end
+
+    figure;
+    imshow(z, []);
+    title(['第 ', num2str(t) + 1, ' - 8 个位平面']);
+    imwrite(z, ['./pic/BMPbit1_8_XtoY/NKU70_gray_hi_', num2str(t), '_rebuild.png']);
 end
+    
